b491508a66c2e05cd3490f5a4b183f574f1742d2
JahidHasanPintu <jahidhasanpintu82@gmail.com>
2022-07-05 19:05:21 +0600

    geolocationt tested

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 
-const displayc= document.getElementById('demo');
+const display= document.getElementById('demo');
 
 function getLocation(){
-    displayc.innerHTML = "working"
-    console.log('working');
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition);
+
+    }else{
+        display.innerHTML= "Your browser doesn't support location";
+    }
+    // displayc.innerHTML = "working"
+    // console.log('working');
+}
+
+function showPosition(position){
+    display.innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude;
 }
